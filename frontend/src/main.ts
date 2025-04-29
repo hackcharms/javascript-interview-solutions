d45e5c47ddd69636885172b8127c013203336417
Zubair Ahmad <hackcharms@gmail.com>
2025-04-29 16:23:44 +0530

    feat(search): Added search todo feature, improved styling as well

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,12 @@
 import { TodoType, useStorage } from './storage/todo';
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { Todo } from './ui/todo';
-import { uuid } from './utils';
-import viteLogo from '/vite.svg'
+import { Todo, getTodoInputElement, getTodoInputFormElement } from './ui/todo';
 export const TODO_SELECTOR = "#todos";
 export const TODO_LIST_SELECTOR = "#todos #todo-list";
 export const TODO_INPUT_FORM_SELECTOR = "#todos #input-form";
 export const TODO_INPUT_SELECTOR = "#todos #input-form #todo-input";
+export const TODO_SEARCH_INPUT_SELECTOR = "#search-input";
+
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <nav class="navbar">
@@ -20,14 +19,14 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <main class="main-wrapper">
       <h2>Tasks</h2>
       <div id="todos">
+      <input type="text" id="search-input" placeholder="Search your Todo"/>
         <form id="input-form" action="">
+        <div class="input-warpper">
           <input type="text" name="todo_name" id="todo-input" placeholder="Enter your Todo"/>
+          <button type="submit" class="btn success">Add</button>
+          </div>
         </form>
         <div id="todo-list">
-          <!-- <div class="item">
-            <button class="title">todo name</button>
-            <button class="cancel">cancel</button>
-          </div> -->
         </div>
       </div>
     </main>
@@ -38,15 +37,24 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     </footer>
 `;
 const TodoStorage = useStorage();
-const inputFormElement = document.querySelector<HTMLFormElement>(TODO_INPUT_FORM_SELECTOR);
+const inputFormElement = getTodoInputFormElement()
 inputFormElement?.addEventListener('submit', (event: SubmitEvent) => {
   event.preventDefault();
-  const input = document.querySelector<HTMLInputElement>(TODO_INPUT_SELECTOR);
+  const input = getTodoInputElement();
   if (!input) return;
-  TodoStorage.add({ title: input.value });
+  TodoStorage.add({ title: input.value.trim() });
   input.value=''
   renderTask(TodoStorage.todos);
 });
+const serachInputElement=document.querySelector<HTMLInputElement>(TODO_SEARCH_INPUT_SELECTOR);
+serachInputElement?.addEventListener('input',(event)=>{
+  const key=(event.target as HTMLInputElement).value.trim();
+  const todos=filterTodo(key)
+  renderTask(todos);
+})
+function filterTodo(text:string){
+    return TodoStorage.todos.filter(({title})=>title.toLowerCase().includes(text.toLowerCase()));
+}
 function renderTask(tasks: TodoType[]) {
   const todo_list = document.querySelector(TODO_LIST_SELECTOR);
   if (!todo_list) return;
@@ -54,21 +62,16 @@ function renderTask(tasks: TodoType[]) {
   const fragment = new DocumentFragment();
   for (let task of tasks) {
     const taskElement = Todo({
-      ...task, cancelAttrs: {
-      }
-    });
-    taskElement
-      .querySelector(".title")
-      ?.addEventListener("click", (event: Event) => {
+      ...task,
+      onTitleCick:() => {
         TodoStorage.update(task.id, { status: "completed" });
         renderTask(TodoStorage.todos);
-      });
-    taskElement
-      .querySelector(".cancel")
-      ?.addEventListener("click", (event: Event) => {
+      },
+      onDelete:() => {
         TodoStorage.update(task.id, { status: "canceled" });
         renderTask(TodoStorage.todos);
-      });
+      }
+    });    
     fragment.append(taskElement);
   }
   todo_list?.append(fragment);
--- frontend/src/ui/todo.ts
+++ frontend/src/ui/todo.ts
@@ -8,13 +8,15 @@ type TodoPropsType = {
   attrs: AttrsType;
   titleAttrs: AttrsType;
   cancelAttrs: AttrsType;
+  onDelete:(task:TodoType)=>void,
+  onTitleCick:(task:TodoType)=>void
 };
 export function serializeHtmlAttrs(attrs: AttrsType) {
   return Object.entries(attrs)
     .map(([key, value]) => `${key}="${value}"`)
     .join(" ");
 }
-export function Todo({ id,title,status="todo", titleAttrs = {}, cancelAttrs = {} }: Partial<TodoPropsType> & TodoType) {
+export function Todo({ id,title,status="todo",onDelete,onTitleCick, titleAttrs = {}, cancelAttrs = {} }: Partial<TodoPropsType> & TodoType) {
     const item = document.createElement('div');
     item.setAttribute('class','item')
     item.setAttribute("id", `todo-item-${id}`);
@@ -26,21 +28,23 @@ export function Todo({ id,title,status="todo", titleAttrs = {}, cancelAttrs = {}
           cancelAttrs
         )} class="cancel">cancel</button>
     `;
+    item.querySelector('button.title')?.addEventListener('click',(e)=>onTitleCick?.({id,title,status}));
+    item.querySelector('button.cancel')?.addEventListener('click',(e)=>onDelete?.({id,title,status}));
     return item;
     
 }
 export function getTodosElement() {
-  return document.querySelector(TODO_SELECTOR);
+  return document.querySelector<HTMLDivElement>(TODO_SELECTOR);
 }
 export function getTodoListElement() {
-  return document.querySelector(TODO_LIST_SELECTOR);
+  return document.querySelector<HTMLDivElement>(TODO_LIST_SELECTOR);
 }
 export function getTodoInputFormElement() {
-  return document.querySelector(TODO_INPUT_FORM_SELECTOR);
+  return document.querySelector<HTMLFormElement>(TODO_INPUT_FORM_SELECTOR);
 }
 export function getTodoInputElement() {
-  return document.querySelector(TODO_INPUT_SELECTOR);
+  return document.querySelector<HTMLInputElement>(TODO_INPUT_SELECTOR);
 }
 export function getTodoItemElement(id: string | number) {
-  return document.querySelector(TODO_LIST_SELECTOR + `#${id}`);
+  return document.querySelector<HTMLDivElement>(TODO_LIST_SELECTOR + `#${id}`);
 }
